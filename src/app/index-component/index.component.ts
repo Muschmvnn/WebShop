94b720c4b2b2c5739036668da7e58594f03f65ef
Marcel Musch <Marcel.Musch@hydro.com>
2017-11-23 12:11:13 +0100

    initial commit

---
+++ src/app/index-component/index.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { OnInit, Inject } from '@angular/core';
+
+@Component({
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.css']
+})
+export class AppIndexComponent implements OnInit {
+    ngOnInit(): void {}
+}
---
+++ src/app/navbar-component/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { OnInit, Inject } from '@angular/core';
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
+})
+export class AppNavbarComponent implements OnInit {
+    ngOnInit(): void {}
+}
